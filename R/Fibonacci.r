763494fee76b1d067679f80b514c4986cb1b35db
Ohnoimded <nivedkrishnaprakash@gmail.com>
2023-05-12 04:22:23 +0530

    modified code, updated README

--- R/Fibonacci.r
+++ R/Fibonacci.r
@@ -7,16 +7,31 @@
 #' fib1 <- fibonacci(50);
 #' @export
 
-
 library(gmp)
-fibonacci <- function(n,save=F,print=T) {
-    fib <- as.bigz(c(0, 1))
-    for (i in 3:n) {
-        fib[i] <- fib[i - 1] + fib[i - 2]
-    }
-    if(save==T){write.table(as.character(fibonacci(n)), "fibonacci.csv", col.names = "Fibonacci Numbers", row.names = FALSE)}
-    if(print==T){fib}
+
+fibonacci <- function(n = 5, save = TRUE, print = FALSE) {
+  fib <- as.bigz(c(0, 1))
+
+  for (i in 3:n) {
+    fib[i] <- fib[i - 1] + fib[i - 2]
+  }
+
+  if (save) {
+    con <- file("fibonacci.csv")
+    cat("Fibonacci Numbers\n", file = con)
+    cat(paste(fib, collapse = "\n"), file = con)
+    close(con)
+    message("Saved as fibonacci.csv")
+    return(invisible())
+  }
+
+  if (print) {
+    print(fib)
+  } else {
+    return(fib)
+  }
 }
+
 
 
 #The function `as.bigz` is used for big numbers and to avoid conversion error while calculating big numbers.<br/>
