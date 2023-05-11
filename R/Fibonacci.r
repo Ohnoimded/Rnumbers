ba280158251904ddc9c0b3d93c71a589e47fe4a6
Ohnoimded <nivedkrishnaprakash@gmail.com>
2023-05-12 02:20:16 +0530

    updated files

--- R/Fibonacci.r
+++ R/Fibonacci.r
@@ -1,18 +1,22 @@
+#' FIbonacci
+#'
+#' Generates large fibonacci sequences
+#' @param fibonacci
+#' @return nth fibonacci number
+#' @examples
+#' fib1 <- fibonacci(50);
+#' @export
+
+
 library(gmp)
-fibonacci <- function(nth) {
+fibonacci <- function(n,save=F,print=T) {
     fib <- as.bigz(c(0, 1))
-    for (i in 3:nth) {
+    for (i in 3:n) {
         fib[i] <- fib[i - 1] + fib[i - 2]
     }
-    fib
+    if(save=T){write.table(as.character(fibonacci(n)), "fibonacci.csv", col.names = "Fibonacci Numbers", row.names = FALSE)}
+    if(print=T){fib}
 }
-n <- 42069
-write.table(as.character(fibonacci(n)), "fibonacci.csv",
-
-
-                col.names = "Fibonacci Numbers", row.names = FALSE)
-# Fibonacci sequence
-### Generate large fibonacci numbers
 
 
 #The function `as.bigz` is used for big numbers and to avoid conversion error while calculating big numbers.<br/>
--- R/Leibniz π.r
+++ R/Leibniz π.r
@@ -1,9 +1,17 @@
+#' Pi
+#'
+#' Generates Pi to varying precision. Default iterations is 10e6.
+#' @param fibonacci
+#' @return Pi
+#' @examples
+#' fib1 <- Pi(digits=50);
+#' @export
+
 # Using a variable input would be faster to find the decimals of pi for small number of digits
-pi <- function() { 
+pi <- function(precision=10e6,digits=50) {
     sum <- 0
-    for (i in 0:10e10) {
+    for (i in 0:precision) {
         sum <- sum + ((-1)^(i)) / ((2 * i)+1) #Leibniz pi/4 formula
     }
-    return(4*sum)
+    return(formatC(4*sum, format = "f", digits = digits))
 }
-print(formatC(pi(), format = "f", digits = 50))
